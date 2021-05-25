23fb844934d3644e67bef11bc2d4482a04e10bf6
Jorge Oliveira <72997126+Darkos32@users.noreply.github.com>
2021-05-24 22:36:09 -0300

    Delete Trabalho_2 directory

--- Trabalho_2/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "java c Leitura.java";
-echo "java c LeitorEscritor.java";
-echo "java c Buffer.java";
-echo "java c Atuadores.java";
-echo "java c Sensores.java";
-echo "java c TesteMonitor.java";
-echo "java c Trabalho2.java";
